78b00bca52b6eea09820dc976f0bf291324ebf9e
Benji Gold <benji.gold@gmail.com>
2024-02-25 10:01:00 -0700

    implemented URL building using the base URL and required URL parameters as URLQueryItems, including one that takes in a string that will be used to search images in the Flickr API

--- FlickrImageSearch/Networking/FlickrImageService.swift
+++ FlickrImageSearch/Networking/FlickrImageService.swift
@@ -9,6 +9,11 @@ import Foundation
 
 class FlickrImageService {
     func fetchImages(searchText: String, completion: @escaping (Result<[FlickrImage], Error>) -> ()) {
-        
+        let baseUrl: String = "https://api.flickr.com/services/feeds/photos_public.gne"
+        let queryItems: [URLQueryItem] = [URLQueryItem(name: "format", value: "json"),
+                                          URLQueryItem(name: "nojsoncallback", value: "1"),
+                                          URLQueryItem(name: "tags", value: searchText)]
+        var urlComponents: URLComponents = URLComponents(string: baseUrl)!
+        urlComponents.queryItems = queryItems
     }
 }
