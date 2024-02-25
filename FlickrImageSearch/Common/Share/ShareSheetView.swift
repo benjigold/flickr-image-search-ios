15ede0b2c3192b8a3a27faf3693c8abaed03094d
Benji Gold <benji.gold@gmail.com>
2024-02-25 12:05:09 -0700

    added and implemented makeUIViewController and updateUIViewController delegate methods

--- FlickrImageSearch/Common/Share/ShareSheetView.swift
+++ FlickrImageSearch/Common/Share/ShareSheetView.swift
@@ -9,5 +9,12 @@ import SwiftUI
 import UIKit
 
 struct ShareSheetView: UIViewControllerRepresentable {
+    var items: [Any]
     
+    func makeUIViewController(context: Context) -> UIActivityViewController {
+        let controller = UIActivityViewController(activityItems: items, applicationActivities: nil)
+        return controller
+    }
+    
+    func updateUIViewController(_ uiViewController: UIActivityViewController, context: Context) {}
 }
