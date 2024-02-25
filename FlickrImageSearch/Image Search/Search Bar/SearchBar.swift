024b9678346def2d099e6fbcb6550b61a90d19ff
Benji Gold <benji.gold@gmail.com>
2024-02-25 10:41:32 -0700

    implemented makeUIView and updateUIView UIViewRepresentable delegate methods; (need to create the coordinator used to set the searchBar delegate)

--- FlickrImageSearch/Image Search/Search Bar/SearchBar.swift
+++ FlickrImageSearch/Image Search/Search Bar/SearchBar.swift
@@ -10,4 +10,13 @@ import SwiftUI
 struct SearchBar: UIViewRepresentable {
     @Binding var text: String
     
+    func makeUIView(context: UIViewRepresentableContext<SearchBar>) -> UISearchBar {
+        let searchBar: UISearchBar = UISearchBar(frame: .zero)
+        searchBar.delegate = context.coordinator // TODO: create coordinator
+        return searchBar
+    }
+
+    func updateUIView(_ uiView: UISearchBar, context: UIViewRepresentableContext<SearchBar>) {
+        uiView.text = text
+    }
 }
