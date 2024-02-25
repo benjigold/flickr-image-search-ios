2d370357c88042a79dcc18550fe3379ec18adf04
Benji Gold <benji.gold@gmail.com>
2024-02-25 12:20:14 -0700

    added String extension with extractImageDimensions() method

--- FlickrImageSearch/Common/Extension/String+ImageDimension.swift
+++ FlickrImageSearch/Common/Extension/String+ImageDimension.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+extension String {
+    func extractImageDimensions() -> (width: String, height: String) {
+        // Example regex pattern to find width and height (adjust based on actual HTML structure)
+        let pattern = "width=\"(\\d+)\".*height=\"(\\d+)\""
+        
+        if let regex = try? NSRegularExpression(pattern: pattern, options: .dotMatchesLineSeparators) {
+            let nsrange = NSRange(self.startIndex..<self.endIndex, in: self)
+            if let match = regex.firstMatch(in: self, options: [], range: nsrange) {
+                let widthRange = Range(match.range(at: 1), in: self)
+                let heightRange = Range(match.range(at: 2), in: self)
+                
+                let width = widthRange.map { String(self[$0]) } ?? "Unknown"
+                let height = heightRange.map { String(self[$0]) } ?? "Unknown"
+                
+                return (width, height)
+            }
+        }
+        
+        return ("Unknown", "Unknown")
+    }
+}
