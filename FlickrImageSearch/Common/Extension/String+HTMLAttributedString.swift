ec715e9a8fede5edf354e9914887a94593aa8db1
Benji Gold <benji.gold@gmail.com>
2024-02-25 12:12:53 -0700

    added String extension with convertHTMLToAttributedString() method

--- FlickrImageSearch/Common/Extension/String+HTMLAttributedString.swift
+++ FlickrImageSearch/Common/Extension/String+HTMLAttributedString.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+extension String {
+    func convertHTMLToAttributedString(completion: @escaping (AttributedString) -> Void) {
+        DispatchQueue.global(qos: .userInitiated).async {
+            guard let data = self.data(using: .utf8) else {
+                DispatchQueue.main.async {
+                    completion(AttributedString())
+                }
+                return
+            }
+
+            if let attributedString = try? NSAttributedString(
+                data: data,
+                options: [.documentType: NSAttributedString.DocumentType.html, .characterEncoding: String.Encoding.utf8.rawValue],
+                documentAttributes: nil) {
+                
+                let attributedString = AttributedString(attributedString)
+                DispatchQueue.main.async {
+                    completion(attributedString)
+                }
+            } else {
+                DispatchQueue.main.async {
+                    completion(AttributedString())
+                }
+            }
+        }
+    }
+}
