7131508a4cf7211971c460b258944dce0ba6245d
Benji Gold <benji.gold@gmail.com>
2024-02-25 11:11:12 -0700

    added a FlickrImageViewModel property in the ContentView with the @StateObject property wrapper

--- FlickrImageSearch/Image Search/ContentView.swift
+++ FlickrImageSearch/Image Search/ContentView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var viewModel: FlickrImageViewModel = FlickrImageViewModel()
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
