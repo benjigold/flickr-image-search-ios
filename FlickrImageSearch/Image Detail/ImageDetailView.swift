da1554f62f527ebb9628830feb61a106d84fef6e
Benji Gold <benji.gold@gmail.com>
2024-02-25 11:36:09 -0700

    added flickImage property; added body with VStack in a ScrollView that uses AsyncImage to asynchronously get and display the FlickrImage, as well as display the description, author, and published date in Text fields

--- FlickrImageSearch/Image Detail/ImageDetailView.swift
+++ FlickrImageSearch/Image Detail/ImageDetailView.swift
@@ -8,5 +8,22 @@
 import SwiftUI
 
 struct ImageDetailView: View {
+    let flickrImage: FlickrImage
     
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading, spacing: 10) {
+                AsyncImage(url: URL(string: flickrImage.media?.m ?? ""))
+                    .aspectRatio(contentMode: .fit)
+                Text(flickrImage.title ?? "No Title")
+                    .bold()
+                Text(flickrImage.description ?? "No Description")
+                    .font(.caption)
+                Text("Author: \(flickrImage.author ?? "Unknown")")
+                Text("Published: \(flickrImage.published ?? "Unknown")")
+            }
+            .padding()
+        }
+        .navigationBarTitle(Text(flickrImage.title ?? "No Title"), displayMode: .inline)
+    }
 }
