dc544897728fe733c7955ae0b84c8ef6cd6bb8fc
Benji Gold <benji.gold@gmail.com>
2024-02-25 12:27:56 -0700

    created String extension with formatDate() method that formats the date as "MMM d, yyyy 'at' h:mm a"

--- FlickrImageSearch/Common/Extension/String+FormatDate.swift
+++ FlickrImageSearch/Common/Extension/String+FormatDate.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+extension String {
+    func formatDate() -> String {
+        let inputFormatter = ISO8601DateFormatter()
+        inputFormatter.formatOptions = [.withInternetDateTime,
+                                        .withDashSeparatorInDate,
+                                        .withColonSeparatorInTime]
+
+        let outputFormatter = DateFormatter()
+        outputFormatter.dateFormat = "MMM d, yyyy 'at' h:mm a"
+        
+        if let date = inputFormatter.date(from: self) {
+            return outputFormatter.string(from: date)
+        } else {
+            return "Invalid Date"
+        }
+    }
+}
