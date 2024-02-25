9a5a8a0681229d2a7185383c5d83541c5a013be4
Benji Gold <benji.gold@gmail.com>
2024-02-25 09:51:22 -0700

    added FlickrImageFeed struct with properties; conforms to Decodable protocol

--- FlickrImageSearch/Networking/FlickrImageFeed.swift
+++ FlickrImageSearch/Networking/FlickrImageFeed.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+struct FlickrImageFeed: Decodable {
+    let title: String?
+    let link: String?
+    let description: String?
+    let modified: String?
+    let generator: String?
+    let items: [FlickrImage]?
+}
