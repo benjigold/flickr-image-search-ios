a432430b7f612f7642005c5551b85ff51d0b5214
Benji Gold <benji.gold@gmail.com>
2024-02-25 09:41:16 -0700

    created Media struct that conforms to Decodable protocol; added Media? optional property to FlickrImage struct

--- FlickrImageSearch/Flickr Image/FlickrImage.swift
+++ FlickrImageSearch/Flickr Image/FlickrImage.swift
@@ -11,8 +11,13 @@ struct FlickrImage: Identifiable {
     let id: UUID = UUID()
     let title: String?
     let link: String?
+    let media: Media?
     let dateTaken: String?
     let description: String?
     let published: String?
     let author: String?
+}
+
+struct Media: Decodable {
+    let m: String?
 }
