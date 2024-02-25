487c42e5b34921c07f7b9d901c13e4078b6cae70
Benji Gold <benji.gold@gmail.com>
2024-02-25 09:33:05 -0700

    initial commit

---
+++ FlickrImageSearch/FlickrImageSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrImageSearchApp.swift
+//  FlickrImageSearch
+//
+//  Created by Benji Gold on 2/25/24.
+//
+
+import SwiftUI
+
+@main
+struct FlickrImageSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
