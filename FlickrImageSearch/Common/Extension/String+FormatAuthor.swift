66411ec87ee15e528558970ecd206620ff5b6d95
Benji Gold <benji.gold@gmail.com>
2024-02-25 12:31:31 -0700

    added String extension with formatAuthor() method to extract the author's name and remove other elements (e.g. email address, quotation marks)

--- FlickrImageSearch/Common/Extension/String+FormatAuthor.swift
+++ FlickrImageSearch/Common/Extension/String+FormatAuthor.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+extension String {
+    func formatAuthor() -> String? {
+        let pattern = "\"([^\"]+)\""
+        
+        if let regex = try? NSRegularExpression(pattern: pattern, options: []) {
+            let nsrange = NSRange(self.startIndex..<self.endIndex, in: self)
+            
+            if let match = regex.firstMatch(in: self, options: [], range: nsrange) {
+                let matchRange = match.range(at: 1)
+                if let swiftRange = Range(matchRange, in: self) {
+                    return String(self[swiftRange])
+                }
+            }
+        }
+        return nil
+    }
+}
